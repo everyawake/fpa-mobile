a2bf0c2c7c1081036178a6da3232698e97d83ace
GenesisSam <samuel940401@gmail.com>
2019-10-05 18:05:00 -0700

    Add fpa send data

--- lib/pages/fingerAuth.dart
+++ lib/pages/fingerAuth.dart
@@ -1,10 +1,13 @@
 import 'dart:ffi';
 import 'dart:io';
 
+import 'package:FPA/env.dart';
+import 'package:FPA/helpers/authToken.dart';
 import 'package:FPA/models/pushNotification.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter/widgets.dart';
+import 'package:http/http.dart' as http;
 import 'package:local_auth/local_auth.dart';
 
 class FingerAuthPage extends StatelessWidget {
@@ -41,6 +44,7 @@ class FingerAuthPage extends StatelessWidget {
     List<BiometricType> availableBiometrics =
         await _auth.getAvailableBiometrics();
     bool isAuthenticated = false;
+    String authState = "no-auth-module";
 
     if (Platform.isAndroid) {
       if (availableBiometrics.contains(BiometricType.fingerprint)) {
@@ -56,7 +60,44 @@ class FingerAuthPage extends StatelessWidget {
       }
     }
 
-    print("!!!! AUTH DONE: ${isAuthenticated}");
+    if (isAuthenticated) {
+      authState = "user-auth-done";
+    } else {
+      authState = "user-auth-failed";
+    }
+
+    await _sendAuthData(authState);
+
     Navigator.of(context).pop();
   }
+
+  _sendAuthData(String authState) async {
+    var url = API_ENDPOINT + "/otid/fpa-auth-send";
+    var client = new http.Client();
+    var _userToken = await AuthTokenStorage().getAuthToken();
+
+    var fpaData =
+        FpaSendData(channelId: this.notiData.socketId, authStatus: authState);
+
+    try {
+      await client.post(url,
+          headers: {
+            "fpa-authenticate-token": _userToken,
+          },
+          body: fpaData);
+    } catch (err) {
+      client.close();
+      print("!!!! err ${err}");
+    }
+  }
+}
+
+class FpaSendData {
+  final String channelId;
+  final String authStatus;
+
+  FpaSendData({
+    this.channelId,
+    this.authStatus,
+  });
 }
